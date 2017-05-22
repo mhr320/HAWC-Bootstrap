7a52fce7953247556dfc86d2132fd4631087115e
Michael H. Roberts <mhr320@gmail.com>
2017-05-22 09:23:05 -0400

    created back to top button

--- js/hawc.js
+++ js/hawc.js
@@ -13,3 +13,13 @@ $('.checkbox').change(function(){
         $(this).button('loading');
     });
 });
+	// sets scroll point to top of page
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+	// listener for button click and fires the topFunction
+$('#backToTop').click(function() {
+	topFunction();
+	$( "#usr" ).focus(); //sets focus back on Full Name field
+})
